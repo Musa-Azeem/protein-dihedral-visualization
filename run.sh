d6239eb3be10e85f36e51c97524ee356840f3f7e
Musa Azeem <musa.mazeem@gmail.com>
2024-05-14 18:52:21 -0400

    multiple

--- run.sh
+++ run.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-for i in {5..25..5}
+
+source .venv/bin/activate
+
+# for i in {5..25..5}
+# do
+#     for j in 0.1 none
+#     do
+#         python3 script.py 4 5 $i $j
+#         python3 script.py 4 6 $i $j
+#         python3 script.py 5 6 $i $j
+#     done
+# done
+
+for i in T1058 T1049 T1056 T1030 T1038
 do
-    for j in 0.1 none
-    do
-        python3 script.py 4 5 $i $j
-        python3 script.py 4 6 $i $j
-        python3 script.py 5 6 $i $j
-    done
+    python3 script.py 5 6 $i
 done
